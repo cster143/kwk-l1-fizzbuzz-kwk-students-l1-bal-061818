f0fbee5aaa41bf91300f1ddf5d5729aaed95c4ab
cster143 <cster143@gmail.com>
2018-06-19 19:33:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,8 @@
 def fizz_buzz(int)
   if int%3 == 0
     puts "Fizz"
+    elsif int%5 == 0
+      puts "Buzz"
+    end
   end
-end
-def fizz_buzz(int)
-  elsif int%5 == 0
-    puts "Buzz"
-  end
-end
 fizz_buzz(20)
